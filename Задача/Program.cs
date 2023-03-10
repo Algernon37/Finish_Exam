5c35ea1e3f71053142acbbc4e46873c8a289fee7
Ivan Esin <ivan.esin.78@mail.ru>
2023-03-10 14:33:16 +0400

    First commit

---
+++ Задача/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Сколько эллементов в массиве будет?");
+int a = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[a];
+string[] array2 = new string[new Random().Next(0,4)];
+
+void FullArray(string [] array)
+{
+Console.WriteLine("Введите элементы массива:");
+for (int i = 0; i < a; i++)
+{
+   string name = Console.ReadLine();
+   array [i] = name;
+}
+}
+
+
+
+FullArray(array1);
+
