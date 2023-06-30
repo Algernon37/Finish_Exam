dc7dbe85eb2fd04feb18c4e69c42a03fa4f28b6e
Ivan Esin <ivan.esin.78@mail.ru>
2023-06-30 21:09:43 +0400

    Работа с git

---
+++ Project1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+double [,] table = new double [n,n];
+int sum = 0;
+int sum1 = 0;
+void FillArray(double[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            sum = sum + 1;
+            sum1 = sum1 + 1;
+            matrix[0,columns] = sum;
+            matrix[rows,0] = sum1;
+            Console.Write($"{matrix[rows,columns]} ");
+        }
+        Console.WriteLine();
+    } 
+}
+FillArray(table);
